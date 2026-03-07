c8bd8f2920b28865637041189e5bad4cb37a5260
Zeenat K Malik <zeenatkm301@gmail.com>
2026-03-07 21:16:58 +0530

    caching page api(main)

--- app/page.tsx
+++ app/page.tsx
@@ -1,61 +1,16 @@
 import EventCard from '@/components/EventCard'
 import ExploreBtn from '@/components/ExploreBtn'
 import { IEvent } from '@/database'
+import { cacheLife } from 'next/cache'
 
-// const events = [
-//   {
-//     image: '/images/event1.png',
-//     title: 'Hack the Future 2024',
-//     slug: 'event-1',
-//     location: 'location-1',
-//     date: 'date-1',
-//     time: 'time-1'
-//   },
-//   {
-//     image: '/images/event2.png',
-//     title: 'AI Innovators Summit',
-//     slug: 'event-1',
-//     location: 'location-1',
-//     date: 'date-1',
-//     time: 'time-1'
-//   },
-//   {
-//     image: '/images/event3.png',
-//     title: 'AI Innovators Summit',
-//     slug: 'event-1',
-//     location: 'location-1',
-//     date: 'date-1',
-//     time: 'time-1'
-//   },
-//   {
-//     image: '/images/event4.png',
-//     title: 'AI Innovators Summit',
-//     slug: 'event-1',
-//     location: 'location-1',
-//     date: 'date-1',
-//     time: 'time-1'
-//   },
-//   {
-//     image: '/images/event5.png',
-//     title: 'AI Innovators Summit',
-//     slug: 'event-1',
-//     location: 'location-1',
-//     date: 'date-1',
-//     time: 'time-1'
-//   },
-//   {
-//     image: '/images/event6.png',
-//     title: 'AI Innovators Summit',
-//     slug: 'event-1',
-//     location: 'location-1',
-//     date: 'date-1',
-//     time: 'time-1'
-//   },
-// ]
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 const page = async () => {
+  'use cache';
+  cacheLife('hours') // Cache the result of this function for 60 seconds, so that if there are multiple requests to the homepage within 60 seconds, it will serve the cached version instead of fetching the events from the API again, improving performance and reducing load on the server.
+  
   const response = await fetch(`${BASE_URL}/api/events`)
   const { events } = await response.json()
+  
   return (
     <section>
       <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
