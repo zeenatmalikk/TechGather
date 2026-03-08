8766d9a7b458de7de727a35d6460e6e014b53de2
Zeenat K Malik <zeenatkm301@gmail.com>
2026-03-08 19:38:38 +0530

    added suspense

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,16 +1,18 @@
 import EventDetails from '@/components/EventDetails';
 import Image from 'next/image';
 import { notFound } from 'next/navigation'
-import React from 'react'
+import React, { Suspense } from 'react'
 
-const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }>}) => {
+const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const slug = params.then((p) => p.slug);
 
 
   return (
     <section id='event'>
+      <Suspense fallback={<div>Loading...</div>}>
 
-      <EventDetails params={slug}/>
+        <EventDetails params={slug} />
+      </Suspense>
     </section>
   )
 }
